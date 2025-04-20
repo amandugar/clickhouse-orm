043a87cf33b6720543a51b54887c3f124e41ee1e
Aman Dugar <amanamandugar@gmail.com>
2025-04-21 00:42:56 +0530

    Npm Updates

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { Model } from "./models/model"
+import { QueryBuilder } from "./models/query-builder"
+import {
+  ConnectionManager,
+  ConnectionCredentials,
+  ConnectionConfig,
+} from "./utils/connection-manager"
+import { MigrationService } from "./services/MigrationService"
+import { MigrationRunner } from "./services/MigrationRunner"
+
+export {
+  Model,
+  QueryBuilder,
+  ConnectionManager,
+  ConnectionCredentials,
+  ConnectionConfig,
+  MigrationService,
+  MigrationRunner,
+}
+
+// Export types
+export * from "./models/fields/base-field"
+export * from "./models/definitions/table-definition"
+export * from "./models/model"
+export * from "./@types"
