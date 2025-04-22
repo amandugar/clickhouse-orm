a6f703f58de5ebc3fe766fc5989b55aad34a882e
Aman Dugar <amanamandugar@gmail.com>
2025-04-22 22:23:41 +0530

    Updates in Bundling

--- src/index.ts
+++ src/index.ts
@@ -9,32 +9,11 @@ import { StringTypes } from './models/fields/string-field'
 import { BooleanTypes } from './models/fields/boolean-field'
 
 // Export all types first
-<<<<<<< HEAD
 export * from './models'
 export * from './services'
 export * from './utils'
 // Export main classes and types
 export { Model, ConnectionManager, ConnectionCredentials, ConnectionConfig }
-=======
-export * from './models/fields/base-field'
-export * from './models/types/table-definition'
-export * from './models/fields/boolean-field'
-export * from './models/fields/number-field'
-export * from './models/fields/string-field'
-export * from './models/fields/array-field'
-export * from './models/fields/tuple-field'
-export * from './services/types'
-
-// Export main classes and types
-export {
-  Model,
-  ConnectionManager,
-  ConnectionCredentials,
-  ConnectionConfig,
-  MigrationService,
-  MigrationRunner,
-}
->>>>>>> c35c6461abe8813529bd14b99b6c4926efd428d0
 
 export const Types = {
   Number: NumberTypes,
